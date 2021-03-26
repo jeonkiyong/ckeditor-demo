c6777f171b682a97485aa621a803b1f72979bffd
jeonkiyong <jeonkiyong111@gmail.comgit config --global user.email jeonkiyong111@gmail.com>
2021-03-26 22:22:05 +0900

    Upload

---
+++ ckeditor/config.js
@@ -0,0 +1,6 @@
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbar = [
+		{ name: '1', items: [ 'Font', 'FontSize', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', 'Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat' ] },
+		{ name: '2', items: [ 'Image', 'Table', 'TextColor', 'BGColor', 'NumberedList', 'BulletedList', 'Blockquote', 'Link', 'Source', 'Maximize' ] }
+	];
+};
