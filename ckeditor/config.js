d8977fe68e5a0792afe660e6713e9d047a734a02
jeonkiyong <jeonkiyong111@gmail.com>
2021-03-27 10:18:02 +0900

    Update

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -1,6 +1,7 @@
 CKEDITOR.editorConfig = function( config ) {
+	config.extraPlugins = 'youtube';
 	config.toolbar = [
 		{ name: '1', items: [ 'Font', 'FontSize', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', 'Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat' ] },
-		{ name: '2', items: [ 'Image', 'Table', 'TextColor', 'BGColor', 'NumberedList', 'BulletedList', 'Blockquote', 'Link', 'Source', 'Maximize' ] }
+		{ name: '2', items: [ 'Image', 'Table', 'TextColor', 'BGColor', 'NumberedList', 'BulletedList', 'Blockquote', 'Link', 'Youtube', 'Source', 'Maximize' ] }
 	];
 };
